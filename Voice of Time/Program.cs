0bfe92c53ef4ac29956246482c1198ee67e15066
BuildTools <unconfigured@null.spigotmc.org>
2022-11-22 19:49:34 +0100

    MoinMoin

--- Voice of Time/Program.cs
+++ Voice of Time/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var integer1 = Console.ReadLine();
+//Convert integer1 to int
+var sadFace = Convert.ToInt32(integer1);
+sadFace++;
+Console.WriteLine(sadFace);
