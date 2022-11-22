687a9de1d529592d7199017fa1a58dcff3516e67
Tim <tim.goetzelmann@web.de>
2022-11-22 19:52:29 +0100

    Test

--- Voice of Time/Program.cs
+++ Voice of Time/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var input = Console.ReadLine();
+//Convert input to int
+var inputInt = Convert.ToInt32(input);
