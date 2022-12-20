bf04e72308d6fe1175dec61ed413c979e805e631
Tim <tim.goetzelmann@web.de>
2022-12-20 23:10:01 +0100

    New Client Class

---
+++ Voice of Time/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace Voice_of_Time
+{
+    /// <summary>
+    /// Server bound Client Identity
+    /// </summary>
+    internal class Client
+    {
+        public long UserID { get; }
+        public string UserName { get; }
+        public RSA UserKey { get; } 
+        public Dictionary<long, byte[]> PublicKeyDictionary { get; }
+
+        public Client(long userID, string userName, RSA userKey, Dictionary<long, byte[]>? publicKeyDictionary = null)
+        {
+            UserID              = userID;
+            UserName            = userName            ?? throw new ArgumentNullException(nameof(userName));
+            UserKey             = userKey             ?? throw new ArgumentNullException(nameof(userKey));
+            PublicKeyDictionary = publicKeyDictionary ?? new();
+        }
+    }
+}
