9c364a7af3f3c5020470dfa9abe75885ff1b116e
Tim <tim.goetzelmann@web.de>
2022-12-21 23:16:18 +0100

    Added TextChats to Client and global using of Core

--- Voice of Time/Client.cs
+++ Voice of Time/Client.cs
@@ -10,14 +10,16 @@ namespace Voice_of_Time
         public long UserID { get; }
         public string UserName { get; }
         public RSA UserKey { get; } 
-        public Dictionary<long, byte[]> PublicKeyDictionary { get; }
+        public Dictionary<long, RSA> PublicKeyDictionary { get; }
+        public List<TextChat> TextChats { get; }
 
-        public Client(long userID, string userName, RSA userKey, Dictionary<long, byte[]>? publicKeyDictionary = null)
+        public Client(long userID, string userName, RSA userKey, Dictionary<long, RSA>? publicKeyDictionary = null, List<TextChat>? textChats = null)
         {
             UserID              = userID;
             UserName            = userName            ?? throw new ArgumentNullException(nameof(userName));
             UserKey             = userKey             ?? throw new ArgumentNullException(nameof(userKey));
             PublicKeyDictionary = publicKeyDictionary ?? new();
+            TextChats           = textChats           ?? new();
         }
     }
 }
