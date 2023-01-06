e5ddcbdd300f174610b274cc377d21582927373c
abeer <abeersenah@gmail.com>
2023-01-06 09:34:28 +0100

    MessageStatus and generating Hash

---
+++ VoTCore/Communication/MessageStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoTCore.Package;
+
+public abstract class MessageStatus {
+
+    public bool status_sent { get; }
+
+    public bool status_received { get; }
+
+    public bool status_failed { get; }
+
+
+    //Message needs generatedHash
+    //sentMessage needs to be compred to the receivedMessage
+    //how do we keep them apart?
+
+    var generatedHash = x.GetHashCode();
+
+    if (generatedHash.Equals(receivedHash))
+        return true;
+
+    if (Message.Equals(generatedHash))
+        return status_sent = true;
+
+
+
+}
