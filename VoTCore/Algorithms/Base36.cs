cb682f546c86a95f1ef8cd1f441abbe2234491a8
Timeplex <72736935+TLTimeplex@users.noreply.github.com>
2023-02-01 16:25:58 +0100

    Add Base36 Coding system for later userID usesage

---
+++ VoTCore/Algorithms/Base36.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoTCore.Algorithms
+{
+    public static class Base36
+    {
+        const string BaseAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const char   Filler       = '=';
+
+        public static string Encode(long value)
+        {
+            string result  = string.Empty;
+            int baseLenght = BaseAlphabet.Length;
+
+            if (value < 0) return "";
+
+            do
+            {
+                result = BaseAlphabet[(int)(value % baseLenght)] + result;
+                value /= baseLenght;
+            } while (value > 0);
+
+            return result;
+        }
+
+        public static string Encode(long value, int minLenght)
+        {
+            string result = Encode(value);
+
+            for(int i = minLenght - result.Length; i > 0; i--)
+            {
+                result += Filler;
+            }
+
+            return result;
+        }
+
+
+        public static long Decode(string value)
+        {
+            value = value.ToUpper();
+
+            value = new string(value.Where(x => BaseAlphabet.Contains(x)).ToArray()) ?? "";
+
+            long result    = 0;
+            int baseLenght = BaseAlphabet.Length;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                result = result * baseLenght + BaseAlphabet.IndexOf(value[i]);
+            }
+
+            return result;
+        }
+
+    }
+}
